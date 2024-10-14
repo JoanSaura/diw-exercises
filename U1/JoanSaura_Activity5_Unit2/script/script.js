05d4f4baf1d6de817b21747dedc447f4d3ce8e44
TommyDrug <jsaura20489@iesjoanramis.org>
2024-10-14 20:24:05 +0200

    14.10

---
+++ U1/JoanSaura_Activity5_Unit2/script/script.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const closeBtn = document.querySelector("#lateral .close");
+    const lateralMenu = document.getElementById("lateral");
+    let isMenuOpen = true;
+  
+    closeBtn.addEventListener("click", function() {
+      if (isMenuOpen) {
+        lateralMenu.style.left = "-170px";
+        closeBtn.innerHTML = '<i class="fa-solid fa-bars"></i>';
+      } else {
+        lateralMenu.style.left = "0";
+        closeBtn.innerHTML = '<i class="fa-solid fa-times"></i>';
+      }
+      isMenuOpen = !isMenuOpen;
+    });
+});
