2591c316e16266339c742fcde039cd612078f17a
JoanSaura <136345085+JoanSaura@users.noreply.github.com>
2024-12-02 20:53:20 +0100

    2/12

--- Examen Irene/tailwind.config.js
+++ Examen Irene/tailwind.config.js
@@ -1,12 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./**/*.{html,js}"], // Rango de archivos donde se buscarán las clases de Tailwind
+  content: ["./**/*.{html,js}"],
   theme: {
     extend: {
       screens: {
-        'tablet': '640px', // Min-width 640px para dispositivos tablet
-        'laptop': '1024px', // Min-width 1024px para laptops
-        'desktop': '1280px', // Min-width 1280px para desktops
+    
+      },
+      variables:{
+
+      },
+      fonts:{
+        
       }
     }
   },
